6010579130582265bdfff3a035c742a7f012a859
Juliana Balingasa <juliana.young.balingasa07112001@gmail.com>
2023-02-01 19:35:50 -0500

    New File - Registration Form
    Creating Registration Form for Admin Panel

---
+++ ADMIN PANEL/registration.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>REGISTRATION | ADMIN PANEL</title>
+	<link rel="stylesheet" type="text/css" href="./css/login.css">
+</head>
+<body>
+     <form action="login.php" method="post">
+     	<h2>ADMIN SIGN UP</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+
+        <label>First Name:</label>
+     	<input type="text" name="fname" placeholder="First Name"><br>
+        
+        <label>Middle Name:</label>
+     	<input type="text" name="mname" placeholder="Middle Name"><br>
+        
+        <label>Last Name:</label>
+     	<input type="text" name="lname" placeholder="Last Name"><br>
+        
+        <label>Admin ID:</label>
+     	<input type="text" name="unique_id" placeholder="Admin ID"><br>
+
+     	<label>Email:</label>
+     	<input type="text" name="uname" placeholder="Email"><br>
+
+     	<label>Password:</label>
+     	<input type="password" name="password" placeholder="Password"><br>
+
+     	<button type="submit">SIGN UP</button>
+        <button type="submit">BACK TO LOGIN</button>
+     </form>
+</body>
+</html>
