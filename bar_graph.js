83e1007413243a4b2ee2694febc48c00102c0a94
Kiara Raye Carganillo <81609830+kiararayecarganillo@users.noreply.github.com>
2023-04-11 19:55:26 +0800

    try

---
+++ bar_graph.js
@@ -0,0 +1,103 @@
+var xValues = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    var yValues = [200,100,170,270,250,290,240,210,280,320,450,200];
+
+    new Chart("myChart3", {
+        type: "bar",
+        data: {
+        labels: xValues,
+        datasets: [{
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "rgba(0,0,255,1.0)",
+        borderColor: "#7B89AD",
+        data: yValues
+        }]
+    },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            aspectRatio: 5,
+            legend: {display: false},
+            scales: {
+            yAxes: [{ticks: {min: 0, max:700}}],
+        }
+    }
+});
+
+var xValues = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    var yValues = [200,100,170,270,250,290,240,210,280,320,450,200];
+
+    new Chart("report_student_chart", {
+        type: "bar",
+        data: {
+        labels: xValues,
+        datasets: [{
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "rgba(0,0,255,1.0)",
+        borderColor: "#7B89AD",
+        data: yValues
+        }]
+    },
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            aspectRatio: 3,
+            legend: {display: false},
+            scales: {
+            yAxes: [{ticks: {min: 0, max:700}}],
+        }
+    }
+});
+
+var xValues = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    var yValues = [200,100,170,270,250,290,240,210,280,320,450,200];
+
+    new Chart("report_appointment_chart", {
+        type: "bar",
+        data: {
+        labels: xValues,
+        datasets: [{
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "rgba(0,0,255,1.0)",
+        borderColor: "#7B89AD",
+        data: yValues
+        }]
+    },
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            aspectRatio: 3,
+            legend: {display: false},
+            scales: {
+            yAxes: [{ticks: {min: 0, max:700}}],
+        }
+    }
+});
+
+var xValues = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+    var yValues = [200,100,170,270,250,290,240,210,280,320,450,200];
+
+    new Chart("report_medicine_chart", {
+        type: "bar",
+        data: {
+        labels: xValues,
+        datasets: [{
+        fill: false,
+        lineTension: 0,
+        backgroundColor: "rgba(0,0,255,1.0)",
+        borderColor: "#7B89AD",
+        data: yValues
+        }]
+    },
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            aspectRatio: 3,
+            legend: {display: false},
+            scales: {
+            yAxes: [{ticks: {min: 0, max:700}}],
+        }
+    }
+});
